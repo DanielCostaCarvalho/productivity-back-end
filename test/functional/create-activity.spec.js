3bdbcb311a24a5ad6c6402bc53963d7848e6fcfc
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2020-05-17 01:39:23 -0300

    test(activity): fix activities tests to match new scope names

--- test/functional/create-activity.spec.js
+++ test/functional/create-activity.spec.js
@@ -11,7 +11,7 @@ trait('Auth/Client')
 test('make sure create a activity when valid arguments are provided', async ({ assert, client }) => {
   const user = await User.create({ username: 'test5', password: '123456', email: 'fake5@mail' })
 
-  const project = await Project.create({ user_id: user.id, name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'trabalho' })
+  const project = await Project.create({ user_id: user.id, name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'work' })
 
   const activity = { description: 'test', initial_date: '2020-01-01', final_date: '2020-01-01' }
 
