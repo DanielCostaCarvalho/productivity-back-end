cfd936dd0ed98fa2253c2e10d777d944627384a9
Daniel Costa Carvalho <danielcostacarvalho@gmail.com>
2020-05-17 01:34:04 -0300

    test(project): fix projects tests

--- test/functional/list-projects.spec.js
+++ test/functional/list-projects.spec.js
@@ -11,7 +11,7 @@ trait('Auth/Client')
 test('make sure list all projects', async ({ assert, client }) => {
   const user = await User.create({ username: 'test4', password: '123456', email: 'fake4@mail' })
 
-  const project = await Project.create({ user_id: user.id, name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'trabalho' })
+  const project = await Project.create({ user_id: user.id, name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'work' })
 
   const response = await client.get('/projects')
     .loginVia(user)
--- test/functional/project.spec.js
+++ test/functional/project.spec.js
@@ -10,7 +10,7 @@ trait('Auth/Client')
 test('make sure can create a new project', async ({ client }) => {
   const user = await User.create({username: 'test3', password: '123456', email: 'fake3@mail'})
 
-  const project = {name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'trabalho' }
+  const project = {name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'work' }
   const response = await client.post('/projects')
     .send(project)
     .loginVia(user)
@@ -21,7 +21,7 @@ test('make sure can create a new project', async ({ client }) => {
 })
 
 test('make sure cannot create a new project whithout a token', async ({ client }) => {
-  const project = {name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'trabalho' }
+  const project = {name: 'test', description: 'test test', initial_date: '2020-01-01', final_date: '2020-01-01', scope: 'work' }
   const response = await client.post('/projects')
     .send(project)
     .end()
